00b5816e1d210dc132da0daf7b03b159a6c7e313
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-03-06 23:54:51 +0900

    change data type of country and add user_id

--- db/migrate/20180302062222_create_patient_backgrounds.rb
+++ db/migrate/20180302062222_create_patient_backgrounds.rb
@@ -4,13 +4,14 @@ class CreatePatientBackgrounds < ActiveRecord::Migration
       t.string :name, null: false
       t.date :birthday, null: false #deta型を指定することでdate_selectメソッドにデータ型を対応させる
       t.string :sex, null: false
-      t.string :country, null: false
+      t.integer :country, default: 0, null: false, limit: 1
       t.integer :hight, null: false
       t.integer :weight, null: false
       t.string :occupation, default: ""
       t.string :religion, default: ""
       t.text :notes, default: ""
       t.string :profile_url, default: ""
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
