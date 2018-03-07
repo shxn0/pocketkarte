7b88c9dbaeda4e86cd6bbed5eec0d773eb24a72d
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-03-07 18:16:17 +0900

    fix abug by adding normalize.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( now-ui-kit.css )
-Rails.application.config.assets.precompile += %w( now-ui-kit.js )
