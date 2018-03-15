ea31be8031b995b890b06f7602ed94b1a4b76f1e
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-03-15 17:34:23 +0900

    add new function which is an uploader for medicines

--- app/helpers/medicines_helper.rb
+++ app/helpers/medicines_helper.rb
@@ -1,10 +1,2 @@
 module MedicinesHelper
-
-  # def choose_new_or_edit
-  #   if action_name == 'new'
-  #     confirm_medicines_path
-  #   elsif action_name == 'edit'
-  #     edit_medicine_path
-  #   end
-  # end
 end
