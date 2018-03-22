235271c8565a4cc28be03737e90d6f48c97eaac6
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-03-22 18:10:47 +0900

    add age calculation and fix items to be translatable

--- app/controllers/patient_backgrounds_controller.rb
+++ app/controllers/patient_backgrounds_controller.rb
@@ -1,12 +1,14 @@
 class PatientBackgroundsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_patient_background, only:[:edit, :update, :destroy]
+  before_action :column_names_of_patient_background, only:[:index]
 
   def index
     @patient_backgrounds = PatientBackground.all
     @patient_backgrounds.each do |patient_background|
       if current_user.id == patient_background.user_id
         @patient_background = current_user.patient_background
+        @age_of_patient = age_of_patient
       end
     end
   end
@@ -50,6 +52,27 @@ class PatientBackgroundsController < ApplicationController
 
     def set_patient_background
       @patient_background = PatientBackground.find(params[:id])
+    end
+
+    def age_of_patient
+      birthday = current_user.patient_background.birthday
+      date_format = "%Y%m%d"
+      (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    end
+
+    def column_names_of_patient_background
+      #patient_backgroundの入力項目はconfig/locales/model.ja.ymlから呼び出して他言語切り替えに対応させている
+      #column_namesにはpatient_backgroundの属性を取得して代入し、各項目名はキーを指定して取得
+      column_names = I18n.t('activerecord.attributes.patient_background')
+        @name = column_names[:name]
+        @birthday = column_names[:birthday]
+        @sex = column_names[:sex]
+        @country = column_names[:country]
+        @hight = column_names[:hight]
+        @weight = column_names[:weight]
+        @occupation = column_names[:occupation]
+        @religion = column_names[:religion]
+        @notes = column_names[:notes]
     end
 
 end
