ea564196c480748e082783a5ce5b268013fda650
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-03-30 19:07:21 +0900

    fixing a bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_locale
-
-  def set_locale
-    I18n.locale = locale # デフォルトでは I18n.locale = :ja
-  end
-
-  def locale
-    @locale ||= params[:locale] || I18n.default_locale # 変数の値がnilであれば、右辺の値を代入する
-  end
-
-  def default_url_options(options={})
-    options.merge(locale: locale)
-  end
+  # before_filter :set_locale
+  #
+  # def set_locale
+  #   I18n.locale = locale # デフォルトでは I18n.locale = :ja
+  # end
+  #
+  # def locale
+  #   @locale ||= params[:locale] || I18n.default_locale # 変数の値がnilであれば、右辺の値を代入する
+  # end
+  #
+  # def default_url_options(options={})
+  #   options.merge(locale: locale)
+  # end
 
 end
