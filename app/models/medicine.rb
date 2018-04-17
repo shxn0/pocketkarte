f79a6adc9886a223351967a7a5c129dc7a2b7f8b
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-04-17 21:15:33 +0900

    add memo

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -4,4 +4,8 @@ class Medicine < ActiveRecord::Base
 
   mount_uploader :medicine_img, MedicineUploader
 
+  # mount_uploader  ：画像のアップロード宣言
+  # :medicine_img   ：テーブル内の画像のパスが入っているカラム名
+  # MedicineUploader：MidicineUploaderクラス(ファイル内)の設定を元にアップロードすることを意味する
+
 end
