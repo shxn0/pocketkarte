ab6b06aa17e09e4ab29907cdaf0ac1ece143db4c
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-04-18 15:55:27 +0900

    fix a bug for uploading img on confirm page

--- app/helpers/medicines_helper.rb
+++ app/helpers/medicines_helper.rb
@@ -1,17 +1,13 @@
 module MedicinesHelper
 
-  def
+  def img_comfirm_path
     case Rails.env
+      when 'production'
+        @medicine.medicine_img.url.slice(62..-1)
 
-    when 'production'
-      @medicine.medicine_img.url.slice(63..-1)
-
-    when 'development'
-
+      when 'development'
+        @medicine.medicine_img.url.slice(63..-1)
+    end
   end
-
-
-
-
 
 end
