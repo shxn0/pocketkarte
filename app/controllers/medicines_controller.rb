3faf3215e2a575bef4f613f36e77a20dbf028b93
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2018-04-18 19:26:09 +0900

    fix a bung

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -19,6 +19,7 @@ class MedicinesController < ApplicationController
 
   def confirm
     @medicine = current_user.medicines.build(medicine_params)
+    img_comfirm_path
     render :new if @medicine.invalid?
   end
 
